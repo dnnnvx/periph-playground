1711621a0e80ade887637f14c0a9051e56f88e83
dvvvxx <dotmarcodestefani@gmail.com>
2021-03-21 00:25:22 +0100

    using go-task

--- apply.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Modify it if you code/build from 
-# your laptop and want to export
-# the binary to your RPi via SSH
-
-env GOOS=linux GOARCH=arm GOARM=5 go build -o bin/play \
-&& sftp pi@192.168.1.132:/home/pi/Golab <<< $'put bin/play'
